1b0c545edc1f86f7d8d72a955f783e823e45f6ad
Sergiy <sergey.morenets@gmail.com>
2023-10-22 11:49:52 +0300

    Task 6.2. Add database initializers

--- payment-service/src/main/java/it/discovery/payment/PaymentApplication.java
+++ payment-service/src/main/java/it/discovery/payment/PaymentApplication.java
@@ -1,12 +1,33 @@
 package it.discovery.payment;
 
+import it.discovery.payment.domain.PaymentProvider;
+import it.discovery.payment.persistence.PaymentProviderRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaymentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(
+            PaymentProviderRepository paymentProviderRepository) {
+        return args -> {
+            PaymentProvider provider = new PaymentProvider();
+            provider.setName("Stripe");
+            paymentProviderRepository.save(provider);
+
+//            Customer customer = new Customer();
+//            customer.setId(1);
+//            customer.setName("Sam Newman");
+//            customer.setEmail("sam.newman@gmail.com");
+//            customer.setProvider(provider);
+//            customerRepository.save(customer);
+        };
     }
 }
